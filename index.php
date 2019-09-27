9e89ef959f9208b5b11a807dbfcf87f7878ee7c7
elton869 <elton869@gmail.com>
2019-09-27 16:27:41 +0000

    31 - Criando funções personalizadas

--- index.php
+++ index.php
@@ -11,7 +11,12 @@ $myFilter = new \Twig\TwigFilter('myfilter', function ($string) {
     return "- " . $string;
 });
 
+$myfunction = new \Twig\TwigFunction('myfunction', function($num1, $num2){
+    return $num1 + $num2;
+});
+
 $twig->addFilter($myFilter);
+$twig->addFunction($myfunction);
 
 $content = $twig->render('hello.html.twig', [
     "nome" => 'Elton Fonseca'
